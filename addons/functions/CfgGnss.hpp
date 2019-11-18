9f13f5bd6513564e0bc13f266ff273b7c13b29c8
Salbei <salbei@gruppe-adler.de>
2019-11-18 10:06:34 +0100

    added base cfgGnss.hpp

---
+++ addons/functions/CfgGnss.hpp
@@ -0,0 +1,18 @@
+class cfgGnss {
+    class gps {
+        satellites = 1;
+        precision = 1;
+    };
+    class glonass {
+        satellites = 1;
+        precision = 1;
+    };
+    class galileo {
+        satellites = 1;
+        precision = 1;
+    };
+    class beidou {
+        satellites = 1;
+        precision = 1;
+    };
+};
